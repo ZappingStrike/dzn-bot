ea5134e08eda31290c0ea5e050720815458f1830
ZappingStrike <zappingstrike@gmail.com>
2017-07-13 21:32:55 +0200

    Trying to fix stuff

--- config(Remove the Bracket).js
+++
@@ -1,8 +0,0 @@
-{
-  "token": "TOKEN HERE",
-  "prefix": "!",
-  "ownerid": "ZAPPING STRIKES ID HERE",
-  "modrolename": "Moderator",
-  "adminrolename": "Admin",
-  "dznStaffName": "Dropzone Nation"
-}
